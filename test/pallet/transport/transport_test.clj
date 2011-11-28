99e003dfae0c4ef863720491a7e5d426f370afd5
Hugo Duncan <hugo@hugoduncan.org>
2011-11-28 16:00:25 -0500

    Add missing transport-test file

---
+++ test/pallet/transport/transport_test.clj
@@ -0,0 +1,69 @@
+(ns pallet.transport.transport-test
+  (:use
+   clojure.test)
+  (:require
+   [pallet.common.filesystem :as filesystem]
+   [pallet.transport :as transport]))
+
+(defn test-exec
+  [transport endpoint authorisation options]
+  (transport/with-transport [transport transport]
+    (let [t-state (transport/open transport endpoint authorisation options)]
+      (testing "Default shell"
+        (let [result (transport/exec t-state {:in "ls /; exit $?"} nil)]
+          (is (zero? (:exit result)))
+          (is (re-find #"bin" (:out result)))))
+      ;; this fails for ssh for some reason
+      ;; (testing "Explicit shell"
+      ;;   (let [result (transport/exec
+      ;;                 t-state
+      ;;                 {:execv ["/bin/bash"] :in "ls /; exit $?"} nil)]
+      ;;     (is (zero? (:exit result)))
+      ;;     (is (re-find #"bin" (:out result)))))
+      (testing "Explicit program"
+        (let [result (transport/exec t-state {:execv ["/bin/ls" "/"]} nil)]
+          (is (zero? (:exit result)))
+          (is (re-find #"bin" (:out result)))))
+      (testing "output-f"
+        (let [output (atom "")
+              result (transport/exec
+                      t-state
+                      {:execv ["/bin/ls" "/"]}
+                      {:output-f (partial swap! output str)})]
+          (is (zero? (:exit result)))
+          (is (re-find #"bin" (:out result)))
+          (is (= (:out result) @output))))
+      (testing "Error return"
+        (let [result (transport/exec t-state {:in "this-should-fail"} nil)]
+          (is (not (zero? (:exit result)))))))))
+
+(defn test-send
+  [transport endpoint authorisation options]
+  (transport/with-transport [transport transport]
+    (let [t-state (transport/open transport endpoint authorisation options)]
+      (testing "send-file"
+        (filesystem/with-temp-file [tmp-src "src"]
+          (filesystem/with-temp-file [tmp-dest "dest"]
+            (transport/send-file t-state (.getPath tmp-src) (.getPath tmp-dest))
+            (is (= "src" (slurp tmp-dest))))))
+      (testing "send-text"
+        (filesystem/with-temp-file [tmp-dest "dest"]
+          (transport/send-text t-state "src" (.getPath tmp-dest))
+          (is (= "src" (slurp tmp-dest)))))
+      (testing "receive"
+        (filesystem/with-temp-file [tmp-src "src"]
+          (filesystem/with-temp-file [tmp-dest "dest"]
+            (transport/receive t-state (.getPath tmp-src) (.getPath tmp-dest))
+            (is (= "src" (slurp tmp-dest))))))
+      (testing "send-file with non-existing path"
+        (is
+         (thrown-with-msg?
+           slingshot.Stone #"No such file"
+           (transport/send-file
+            t-state "/some/non-existing/path" "/invalid")))))))
+
+(defn test-connect-fail
+  [transport endpoint authorisation options]
+  (transport/with-transport [transport transport]
+    (let [t-state (transport/open transport endpoint authorisation options)]
+      (is false "this test is designed to fail on open"))))
