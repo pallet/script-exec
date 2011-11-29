af3fcdaff09c37790f5e025197450384cdd95129
Hugo Duncan <hugo@hugoduncan.org>
2011-11-28 23:58:24 -0500

    Factor out ssh-transport and local-transport

--- src/pallet/transport/local.clj
+++ src/pallet/transport/local.clj
@@ -6,67 +6,8 @@
    [clojure.tools.logging :as logging]
    [pallet.common.context :as context]
    [pallet.shell :as shell]
-   [pallet.transport :as transport]))
-
-(def
-  ^{:doc "Specifies the buffer size used to read the output stream.
-    Defaults to 10K"}
-  output-buffer-size (atom (* 1024 10)))
-
-(def
-  ^{:doc "Specifies the polling period for retrieving command output.
-    Defaults to 1000ms."}
-  output-poll-period (atom 1000))
-
-(defn read-buffer [stream output-f]
-  (let [buffer-size @output-buffer-size
-        bytes (byte-array buffer-size)
-        sb (StringBuilder.)]
-    {:sb sb
-     :reader (fn []
-               (when (pos? (.available stream))
-                 (let [num-read (.read stream bytes 0 buffer-size)
-                       s (String. bytes 0 num-read "UTF-8")]
-                   (output-f s)
-                   (.append sb s)
-                   s)))}))
-
-(defn sh-script
-  "Run a script on local machine.
-
-   Command:
-     :execv  sequence of command and arguments to be run (default /bin/bash).
-     :in     standard input for the process.
-   Options:
-     :output-f  function to incrementally process output"
-  [{:keys [execv in] :as command}
-   {:keys [output-f] :as options}]
-  (logging/tracef "sh-script %s" command)
-  (if output-f
-    (try
-      (let [{:keys [out err proc]} (apply
-                                    shell/sh
-                                    (concat
-                                     (or execv ["/bin/bash"]) ;; TODO generalise
-                                     [:in in :async true]))
-            out-reader (read-buffer out output-f)
-            err-reader (read-buffer err output-f)
-            period @output-poll-period
-            read-out (:reader out-reader)
-            read-err (:reader err-reader)]
-        (with-open [out out err err]
-          (while (not (try (.exitValue proc)
-                           (catch IllegalThreadStateException _)))
-            (Thread/sleep period)
-            (read-out)
-            (read-err))
-          (while (read-out))
-          (while (read-err))
-          (let [exit (.exitValue proc)]
-            {:exit exit
-             :out (str (:sb out-reader))
-             :err (str (:sb err-reader))}))))
-    (apply shell/sh (concat (or execv ["/bin/bash"]) [:in in]))))
+   [pallet.transport :as transport]
+   [pallet.local.transport :as local]))
 
 (deftype LocalTransportState []
   transport/TransportState
@@ -75,18 +16,12 @@
   (close [_])
   transport/Transfer
   (send [_ source destination]
-    (context/with-context
-      (format "Send to %s" destination) {}
-      (io/copy source (io/file destination))))
+    (local/send-stream source destination))
   (receive [_ source destination]
-    (context/with-context
-      (format "Receive to %s" destination) {}
-      (io/copy (io/file source) (io/file destination))))
+    (local/send-stream (io/input-stream source) destination))
   transport/Exec
   (exec [transport-state code options]
-    (context/with-context
-      "Execute script" {}
-      (sh-script code options))))
+    (local/exec code options)))
 
 (deftype LocalTransport []
   transport/Transport
--- src/pallet/transport/ssh.clj
+++ src/pallet/transport/ssh.clj
@@ -8,249 +8,44 @@
    [clojure.tools.logging :as logging]
    [pallet.cache :as cache]
    [pallet.common.context :as context]
-   [pallet.transport :as transport]))
-
-(defonce default-agent-atom (atom nil))
-(defn default-agent
-  []
-  (or @default-agent-atom
-      (swap! default-agent-atom
-             (fn [agent]
-               (if agent
-                 agent
-                 (ssh/create-ssh-agent false))))))
-
-(defn possibly-add-identity
-  [agent private-key-path passphrase]
-  (if passphrase
-    (ssh/add-identity agent private-key-path passphrase)
-    (ssh/add-identity-with-keychain agent private-key-path)))
-
-(defn- status-msg
-  [msg endpoint authentication]
-  (format
-   "%s : server %s, port %s, user %s"
-   msg
-   (:server endpoint)
-   (:port endpoint 22)
-   (-> authentication :user :username)))
-
-(defn ssh-user-credentials
-  "Middleware to user the session :user credentials for SSH authentication."
-  [authentication]
-  (let [user (:user authentication)]
-    (logging/infof "SSH user %s %s" (:username user) (:private-key-path user))
-    (possibly-add-identity
-     (default-agent) (:private-key-path user) (:passphrase user))))
-
-(defn connect-ssh-session
-  [ssh-session endpoint authentication]
-  (when-not (ssh/connected? ssh-session)
-    (context/with-logged-context
-      (status-msg "SSH connect" endpoint authentication)
-      {:exception-type :pallet/ssh-connection-failure
-       :exception-map {:endpoint endpoint :authentication authentication}}
-      (ssh/connect ssh-session))))
-
-(defn connect-sftp-channel
-  [sftp-channel endpoint authentication]
-  (when-not (ssh/connected? sftp-channel)
-    (context/with-logged-context
-      (status-msg "SSH connect SFTP channel" endpoint authentication)
-      {:exception-type :pallet/sftp-channel-failure
-       :exception-map {:endpoint endpoint :authentication authentication}}
-      (ssh/connect sftp-channel))))
-
-(defn connect
-  ([endpoint authentication options]
-     (let [ssh-session (ssh/session
-                        (default-agent)
-                        (:server endpoint)
-                        :username (-> authentication :user :username)
-                        :strict-host-key-checking (:strict-host-key-checking
-                                                   options :no)
-                        :port (:port endpoint 22)
-                        :password (-> authentication :user :password))
-           _ (.setDaemonThread ssh-session true)
-           _ (connect-ssh-session ssh-session endpoint authentication)
-           sftp-channel (ssh/ssh-sftp ssh-session)]
-       (connect-sftp-channel sftp-channel endpoint authentication)
-       {:ssh-session ssh-session
-        :sftp-channel sftp-channel
-        :endpoint endpoint
-        :authentication authentication
-        :options options}))
-  ([state]
-     (let [ssh-session (:ssh-session state)
-           _ (connect-ssh-session ssh-session)
-           sftp-channel (:sftp-channel state)]
-       (connect-sftp-channel sftp-channel)
-       state)))
-
-(defn close
-  "Close any ssh connection to the server specified in the session."
-  [{:keys [ssh-session sftp-channel endpoint authentication] :as state}]
-  (context/with-logged-context
-    (status-msg "SSH close" endpoint authentication)
-    {:exception-type :pallet/ssh-close-failure
-     :exception-map {:endpoint endpoint :authentication authentication}}
-    (when sftp-channel
-      (logging/debugf "SSH disconnect SFTP %s" endpoint)
-      (ssh/disconnect sftp-channel))
-    (when ssh-session
-      (logging/debugf "SSH disconnect SSH %s" endpoint)
-      (ssh/disconnect ssh-session))
-    state))
-
-(defn send
-  [{:keys [sftp-channel endpoint authentication] :as state} source destination]
-  (context/with-logged-context
-    (status-msg
-     (format "SSH SFTP send to %s" destination) endpoint authentication)
-    {:exception-type :pallet/sftp-send-failure
-     :exception-map {:endpoint endpoint :authentication authentication}}
-    (ssh/sftp
-     sftp-channel
-     :put source
-     destination
-     :return-map true)))
-
-(defn receive
-  [{:keys [sftp-channel endpoint authentication] :as state} source destination]
-  (context/with-logged-context
-    (status-msg
-     (format "SSH SFTP receive from %s to %s" source destination)
-     endpoint authentication)
-    {:exception-type :pallet/sftp-receive-failure
-     :exception-map {:endpoint endpoint :authentication authentication}}
-    (ssh/sftp
-     sftp-channel :get source (io/output-stream (io/file destination)))))
-
-(def
-  ^{:doc "Specifies the buffer size used to read the ssh output stream.
-    Defaults to 10K, to match clj-ssh.ssh/*piped-stream-buffer-size*"}
-  ssh-output-buffer-size (atom (* 1024 10)))
-
-(def
-  ^{:doc "Specifies the polling period for retrieving ssh command output.
-    Defaults to 1000ms."}
-  output-poll-period (atom 1000))
-
-(defn exec
-  [{:keys [ssh-session sftp-channel endpoint authentication] :as state}
-   {:keys [execv in] :as code}
-   {:keys [output-f pty] :as options}]
-  (logging/tracef "ssh/exec %s" code)
-  (logging/tracef "ssh/exec %s" (pr-str state))
-  (logging/tracef "ssh/exec session connected %s" (ssh/connected? ssh-session))
-  (if output-f
-    (let [[shell stream] (apply
-                          ssh/ssh
-                          ssh-session
-                          (concat
-                           execv
-                           [:in in :return-map true
-                            :pty (:pty options true) :out :stream]))
-          sb (StringBuilder.)
-          buffer-size @ssh-output-buffer-size
-          period @output-poll-period
-          bytes (byte-array buffer-size)
-          read-ouput (fn []
-                       (when (pos? (.available stream))
-                         (let [num-read (.read stream bytes 0 buffer-size)
-                               s (String. bytes 0 num-read "UTF-8")]
-                           (output-f s)
-                           (.append sb s)
-                           s)))]
-      (while (ssh/connected? shell)
-        (Thread/sleep period)
-        (read-ouput))
-      (while (read-ouput))
-      (.close stream)
-      (let [exit (.getExitStatus shell)
-            stdout (str sb)]
-        (if (zero? exit)
-          {:out stdout :exit exit}
-          (do
-            (logging/errorf "Exit status  : %s" exit)
-            {:out stdout :exit exit
-             :error {:message (format
-                               "Error executing script :\n :cmd %s\n :out %s\n"
-                               code stdout)
-                     :type :pallet-script-excution-error
-                     :script-exit exit
-                     :script-out stdout
-                     :server (:server endpoint)}}))))
-    (let [{:keys [out exit] :as result} (apply
-                                         ssh/ssh
-                                         ssh-session
-                                         (concat
-                                          (when-let [execv (seq execv)]
-                                            [(apply
-                                              str
-                                              (interpose " " (map str execv)))])
-                                          [:in in :return-map true
-                                           :pty (:pty options true)]))]
-
-      (if (zero? exit)
-        result
-        (do
-          (logging/errorf "Exit status  : %s" exit)
-          {:out out :exit exit
-           :error {:message (format
-                             "Error executing script :\n :cmd %s\n :out %s\n"
-                             code out)
-                   :type :pallet-script-excution-error
-                   :script-exit exit
-                   :script-out out
-                   :server (:server endpoint)}})))))
-
-(defn forward-to-local
-  [{:keys [ssh-session sftp-channel endpoint authentication] :as state}
-   remote-port
-   local-port]
-  (.setPortForwardingL ssh-session local-port (:server endpoint) remote-port))
-
-(defn unforward-to-local
-  [{:keys [ssh-session sftp-channel endpoint authentication] :as state}
-   remote-port
-   local-port]
-  (.delPortForwardingL ssh-session local-port))
+   [pallet.transport :as transport]
+   [pallet.ssh.transport :as ssh-transport]))
 
 (deftype SshTransportState
     [state]
   transport/TransportState
   (open? [_]
-    (ssh/connected? (:ssh-session state)))
+    (ssh-transport/connected? (:ssh-session state)))
   (re-open [_]
-    (connect state))
+    (ssh-transport/connect state))
   (close [_]
-    (close state))
+    (ssh-transport/close state))
   transport/Transfer
   (send [_ source destination]
-    (send state source destination))
+    (ssh-transport/send-stream state source destination))
   (receive [_ source destination]
-    (receive state source destination))
+    (ssh-transport/receive state source destination))
   transport/Exec
   (exec [transport-state code options]
-    (exec state code options))
+    (ssh-transport/exec state code options))
   transport/PortForward
   (forward-to-local [transport-state remote-port local-port]
-    (forward-to-local transport-state remote-port local-port))
+    (ssh-transport/forward-to-local transport-state remote-port local-port))
   (unforward-to-local [transport-state remote-port local-port]
-    (unforward-to-local transport-state remote-port local-port)))
+    (ssh-transport/unforward-to-local transport-state remote-port local-port)))
 
 (defn lookup-or-create-state
   [cache endpoint authentication options]
   (or
    (get cache [endpoint authentication options])
-   (let [state (SshTransportState. (connect endpoint authentication options))]
+   (let [state (SshTransportState.
+                (ssh-transport/connect endpoint authentication options))]
      (logging/debugf "Create ssh transport state: %s" endpoint)
      (cache/miss cache [endpoint authentication options] state)
      state)))
 
 (defn open [cache endpoint authentication options]
-  (ssh-user-credentials authentication)
+  (ssh-transport/ssh-user-credentials authentication)
   (lookup-or-create-state cache endpoint authentication options))
 
 (deftype SshTransport [connection-cache]
