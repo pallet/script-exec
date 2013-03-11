bd5a65087549a6dc5090c7ed9b19034bffce80be
Hugo Duncan <hugo@hugoduncan.org>
2013-03-11 14:05:17 -0400

    Use lein as build tool
    Also deploy to clojars with com.palletops group id.

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject com.palletops/script-exec "0.3.0-SNAPSHOT"
+  :description "Functions for executing scripts locally."
+  :url "http://palletops.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.logging "0.2.0"]
+                 [com.palletops/local-transport "0.4.0"]
+                 [com.palletops/ssh-transport "0.4.0"]])
