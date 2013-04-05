665a7ecd43905f6bb6b757e65d5bacd3dbdd1699
Hugo Duncan <hugo@hugoduncan.org>
2013-04-05 15:48:14 -0400

    Update to ssh-transport 0.4.1

--- project.clj
+++ project.clj
@@ -6,4 +6,4 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.0"]
                  [com.palletops/local-transport "0.4.0"]
-                 [com.palletops/ssh-transport "0.4.0"]])
+                 [com.palletops/ssh-transport "0.4.1"]])
