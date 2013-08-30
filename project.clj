9a478f24378e74d88d1e7f02bf90a20497ecf527
Hugo Duncan <hugo@hugoduncan.org>
2013-08-29 20:26:18 -0400

    Update transports to 0.5.0

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.0"]
-                 [com.palletops/local-transport "0.4.0"]
-                 [com.palletops/ssh-transport "0.4.5"]])
+                 [com.palletops/local-transport "0.5.0"]
+                 [com.palletops/ssh-transport "0.5.0"]])
