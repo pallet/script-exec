76c5e03cb420ddb6217086192b7458312c11f525
Hugo Duncan <hugo@hugoduncan.org>
2016-04-26 15:10:51 -0400

    Add schema as a direct dependency

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/tools.logging "0.2.0"]
                  [com.palletops/local-transport "0.5.0"]
-                 [com.palletops/ssh-transport "0.5.2"]])
+                 [com.palletops/ssh-transport "0.5.2"]
+                 [prismatic/schema "0.2.1"]])
